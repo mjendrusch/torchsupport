561f62187700eb481b677b75f4265877cd647e60
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2018-07-25 16:54:14 +0200

    fixup.

--- torchsupport/utils/memory.py
+++ torchsupport/utils/memory.py
@@ -8,7 +8,10 @@ def memory_used():
         if torch.is_tensor(obj):
             if type(obj) not in result:
                 result[type(obj)] = 0
-            result[type(obj)] += obj.element_size() * reduce(operator.mul, list(obj.size()))
+            count = 1
+            for elem in list(obj.data.size()):
+                count *= elem
+            result[type(obj.data)] += count * obj.data.element_size()
         elif hasattr(obj, 'data') and torch.is_tensor(obj.data):
             if type(obj.data) not in result:
                 result[type(obj.data)] = 0
