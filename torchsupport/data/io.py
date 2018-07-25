02fd50aa2533ffee144c8e0e1b210b6b4e871d96
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2018-07-25 13:09:39 +0200

    make imread robust against network problems.

--- torchsupport/data/io.py
+++ torchsupport/data/io.py
@@ -1,10 +1,22 @@
 from skimage import io
 import torch
 import numpy as np
+import time
 
 def imread(path, type='float32'):
   """Reads a given image from file, returning a float tensor."""
-  image = io.imread(path)
+  reading = True
+  while reading:
+    try:
+      image = io.imread(path)
+      reading = False
+    except OSError as e:
+      if e.errno == 121:
+        print("Attempting to recover from Remote IO Error ...")
+        time.sleep(10)
+      else:
+        print("Unexpected OSError. Aborting ...")
+        raise e
   image = np.array(image).astype(type)
   image = np.transpose(image,(2,0,1))
   image = torch.from_numpy(image)
