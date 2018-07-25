1243e09cf14c3d74728e0628d0ff5b2c3e237a38
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2018-07-25 16:52:17 +0200

    add memory tracking tools.

---
+++ torchsupport/utils/memory.py
@@ -0,0 +1,20 @@
+import torch
+import gc
+import operator
+
+def memory_used():
+    result = {}
+    for obj in gc.get_objects():
+        if torch.is_tensor(obj):
+            if type(obj) not in result:
+                result[type(obj)] = 0
+            result[type(obj)] += obj.element_size() * reduce(operator.mul, list(obj.size()))
+        elif hasattr(obj, 'data') and torch.is_tensor(obj.data):
+            if type(obj.data) not in result:
+                result[type(obj.data)] = 0
+            count = 1
+            for elem in list(obj.data.size()):
+                count *= elem
+            result[type(obj.data)] += count * obj.data.element_size()
+
+    return result
