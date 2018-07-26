fb83404a06b9aa56a0bcfdeab246d3eb56e25517
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2018-07-26 17:11:47 +0200

    make memory debugging robust to bad objects.

--- torchsupport/utils/memory.py
+++ torchsupport/utils/memory.py
@@ -5,19 +5,22 @@ import operator
 def memory_used():
     result = {}
     for obj in gc.get_objects():
-        if torch.is_tensor(obj):
-            if type(obj) not in result:
-                result[type(obj)] = 0
-            count = 1
-            for elem in list(obj.data.size()):
-                count *= elem
-            result[type(obj.data)] += count * obj.data.element_size()
-        elif hasattr(obj, 'data') and torch.is_tensor(obj.data):
-            if type(obj.data) not in result:
-                result[type(obj.data)] = 0
-            count = 1
-            for elem in list(obj.data.size()):
-                count *= elem
-            result[type(obj.data)] += count * obj.data.element_size()
+        try:
+            if torch.is_tensor(obj):
+                if type(obj) not in result:
+                    result[type(obj)] = 0
+                count = 1
+                for elem in list(obj.data.size()):
+                    count *= elem
+                result[type(obj.data)] += count * obj.data.element_size()
+            elif hasattr(obj, 'data') and torch.is_tensor(obj.data):
+                if type(obj.data) not in result:
+                    result[type(obj.data)] = 0
+                count = 1
+                for elem in list(obj.data.size()):
+                    count *= elem
+                result[type(obj.data)] += count * obj.data.element_size()
+        except:
+            print("could not track ...")
 
     return result
