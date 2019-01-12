5129e2c394b3646c94a9674114c400a25e5d73f1
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2019-01-12 17:09:59 +0100

    deleting nodes and edges, cloning, coloring based graph pooling.

--- torchsupport/modules/nodegraph.py
+++ torchsupport/modules/nodegraph.py
@@ -21,6 +21,20 @@ class NodeGraphTensor(object):
 
       self._node_tensor = graphdesc["node_tensor"]
 
+  def new_like(self):
+    result = NodeGraphTensor()
+    result.is_subgraph = self.is_subgraph
+    result.offset = self.offset
+    result.num_graphs = self.num_graphs
+    result.graph_nodes = deepcopy(self.graph_nodes)
+    result._adjacency = deepcopy(self._adjacency)
+    return result
+
+  def clone(self):
+    result = self.new_like()
+    result._node_tensor = self._node_tensor.clone()
+    return result
+
   @property
   def adjacency(self):
     if self.is_subgraph:
@@ -37,7 +51,7 @@ class NodeGraphTensor(object):
       stop = self.nodes_including(self.offset)
       return self._node_tensor[start:stop]
     else:
-      return self._adjacency
+      return self._node_tensor
 
   @node_tensor.setter
   def node_tensor(self, value):
@@ -62,9 +76,53 @@ class NodeGraphTensor(object):
     return self._node_tensor.size(0) - 1
 
   def add_edge(self, source, target):
-    self.adjacency[source].append(target)
-    self.adjacency[target].append(source)
-    return len(self.adjacency[source]) - 1
+    self._adjacency[source].append(target)
+    self._adjacency[target].append(source)
+    return len(self._adjacency[source]) - 1
+
+  def add_edges(self, edges):
+    for edge in edges:
+      self.add_edge(*edge)
+
+  def delete_nodes(self, nodes):
+    nodes_to_keep = []
+    new_adjacency = []
+    nodes = sorted(nodes)
+    in_graph = 0
+    node_sum = self.nodes_including(in_graph)
+    nodes_removed = 0
+    removed_index = 0
+    for node in range(len(self._adjacency)):
+      if nodes[removed_index] == node:
+        while node > node_sum:
+          self.graph_nodes[in_graph] -= nodes_removed
+          in_graph += 1
+          node_sum = self.nodes_including(in_graph)
+          nodes_removed = 0
+        nodes_removed += 1
+        removed_index += 1
+      else:
+        nodes_to_keep.append(node)
+    for node in range(len(self._adjacency)):
+      new_adjacency.append([
+        nodes_to_keep.index(target)
+        for target in self._adjacency[node]
+        if target in nodes_to_keep
+      ])
+    
+    self._node_tensor = self._node_tensor[nodes_to_keep]
+    self._adjacency = new_adjacency
+
+  def delete_node(self, node):
+    self.delete_nodes([node])
+
+  def delete_edge(self, source, target):
+    self._adjacency[source] = [x in self._adjacency[source] if x != target]
+    self._adjacency[target] = [x in self._adjacency[target] if x != source]
+
+  def delete_edges(self, edges):
+    for edge in edges:
+      self.delete_edge(*edge)
 
   def __getitem__(self, idx):
     assert (self.num_graphs > 1)
@@ -87,6 +145,12 @@ class PartitionedNodeGraphTensor(NodeGraphTensor):
       self.partition = { None: [] }
     else:
       self.partition = graphdesc["partition"]
+
+  def new_like(self):
+    result = super(PartitionedNodeGraphTensor, self).new_like()
+    result.partition_view = self.partition_view
+    result.partition = self.partition
+    return result
 
   def none(self):
     view = copy(self)
@@ -112,6 +176,22 @@ class PartitionedNodeGraphTensor(NodeGraphTensor):
     self.partition[kind].append(node)
     return node
 
+  def delete_nodes(self, nodes):
+    nodes_to_keep = [
+      node
+      for node in range(len(self._adjacency))
+      if node not in nodes
+    ]
+    self.partition = {
+      kind : [
+        nodes_to_keep.index(node)
+        for node in self.partition[kind]
+        if node not in nodes
+      ]
+      for kind in self.partition
+    }
+    super(PartitionedNodeGraphTensor, self).delete_nodes(nodes)
+
   def append(self, graph_tensor):
     for kind in self.partition:
       self.partition[kind] += list(map(
@@ -130,12 +210,14 @@ class AllNodes(nn.Module):
     self.node_update = node_update
 
   def forward(self, graph):
-    if isinstance(graph, PartitionedNodeGraphTensor) and graph.partition_view != None:
-      partition = graph.partition[graph.partition_view]
-      graph._node_tensor[partition, :] = self.node_update(graph._node_tensor[partition, :])
+    out = graph.new_like()
+    if isinstance(out, PartitionedNodeGraphTensor) and out.partition_view != None:
+      out._node_tensor = graph._node_tensor.clone()
+      partition = out.partition[out.partition_view]
+      out._node_tensor[partition, :] = self.node_update(out._node_tensor[partition, :])
     else:
-      graph._node_tensor = self.node_update(graph._node_tensor)
-    return graph
+      out._node_tensor = self.node_update(out._node_tensor)
+    return out
 
 def LinearOnNodes(insize, outsize):
   lin = nn.Linear(insize, outsize)
@@ -182,8 +264,9 @@ class NodeGraphNeighbourhood(nn.Module):
         nodes = self.order(graph, nodes)
       full_nodes.append(nodes)
     reduced_nodes = self.reducer(graph, full_nodes)
-    graph._node_tensor = torch.cat((graph._node_tensor, reduced_nodes), 1)
-    return graph
+    out = graph.new_like()
+    out._node_tensor = torch.cat((graph._node_tensor, reduced_nodes), 1)
+    return out
 
 def _node_neighbourhood_attention(att):
   def reducer(graph, nodes):
@@ -284,3 +367,93 @@ def NodeNeighbourhoodMin(traversal=standard_node_traversal(1)):
     _node_neighbourhood_reducer(torch.Tensor.min),
     traversal=traversal
   )
+
+class ColorPool(nn.Module):
+  def __init__(self, pooling, order=None,
+               coloring=standard_node_coloring(2),
+               traversal=standard_node_traversal(1)):
+    """
+    Generalization of (maximum-) pooling from images to graphs.
+    Chooses a set of pooling centers using a user specified `coloring`,
+    and pools the pooling centers' neighbourhoods generated using a user
+    specified `traversal` according to a `pooling` function.
+    Args:
+      pooling (callable): pooling function.
+      order (callable): function specifying a sort order for nodes to be pooled.
+      coloring (callable): function specifying pooling centers on a graph.
+      traversal (callable): function computing a neighbourhood traversal for a given node.
+    """
+    super(ColorPool, self).__init__()
+    self.pooling = pooling
+    self.order = order
+    self.coloring = coloring
+    self.traversal = traversal
+
+  def _pooled_nodes(self, selected_nodes, neighbourhoods):
+    return [
+      node
+      for node in set(sum(neighbourhoods, []))
+      if node not in selected_nodes
+    ]
+
+  def _expanded_edges(self, graph, nodes_to_delete):
+    return [
+      (node, target)
+      for skip in nodes_to_delete
+      for idx, node in enumerate(graph._adjacency[skip])
+      for target in graph._adjacency[skip][idx+1:]
+      if (node not in nodes_to_delete) and (target not in nodes_to_delete)
+    ]
+
+  def _ordered_neighbourhoods(self, graph, selected_nodes):
+    neighbourhoods = []
+    for node in selected_nodes:
+      nodes = self.traversal(graph, node)
+      if self.order != None:
+        nodes = self.order(graph, nodes)
+      neighbourhoods.append(nodes)
+    return neighbourhoods
+
+  def forward(self, graph):
+    # select pooling centers and compute pooling neighbourhoods:
+    pooling_centers = self.coloring(graph)
+    neighbourhoods = self._ordered_neighbourhoods(graph, pooling_centers)
+
+    # nodes and edges to be amended:
+    nodes_to_delete = self._pooled_nodes(pooling_centers, neighbourhoods)
+    edges_to_add = self._expanded_edges(graph, nodes_to_delete)
+
+    # pool and cull graph:
+    out = graph.clone()
+    out.add_edges(edges_to_add)
+    out.delete_nodes(nodes_to_delete)
+    out._node_tensor = torch.cat([
+      self.pooling(graph._node_tensor[[node] + neighbourhoods[idx]])
+      for idx, node in enumerate(pooling_centers)
+    ], dim=0)
+    return out, pooling_centers
+
+class ColorUnpool(nn.Module):
+  def __init__(self, unpool):
+    """
+    Unpools a graph previously pooled using `ColorPool` by applying
+    a partial inverse unpool operation broadcasting data from pooling
+    centers to pooled nodes.
+    Args:
+      unpool (callable): unpooling operation.
+    """
+    self.unpool = unpool
+
+  def forward(self, input, indices, guide_graph):
+    out = guide_graph.new_like()
+    out._node_tensor = torch.zeros((guide_graph.size(0), *input.size[1:]))
+    node_sum = 0
+    for node, edges in enumerate(guide_graph._adjacency):
+      if node in indices:
+        out._node_tensor[node] = input._node_tensor[indices.index(node)]
+        for target in edges:
+          out._node_tensor[target] = self.unpool(
+            out._node_tensor[target],
+            input._node_tensor[indices.index(node)]
+          )
+    return out
