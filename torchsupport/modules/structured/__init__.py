4244f26b66e9ddb25104a9da235e57df25f0cad0
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2019-05-09 15:09:41 +0200

    fix.

--- torchsupport/modules/structured/__init__.py
+++ torchsupport/modules/structured/__init__.py
@@ -1,6 +1,7 @@
 """TODO"""
 
-from torchsupport.modules.structured.nodegraph import *
+from torchsupport.modules.structured.connected_entities import *
 from torchsupport.modules.structured.pooling import *
 from torchsupport.modules.structured.spectral import *
-from torchsupport.modules.structured.graphnn import *
+from torchsupport.modules.structured.entitynn import *
+from torchsupport.modules.structured.entitystructure import *
