05c4d36ff9fa7012cac6f189a2ebce99ec849a08
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2019-06-28 18:01:48 +0200

    add missing file.

--- torchsupport/modules/structured/__init__.py
+++ torchsupport/modules/structured/__init__.py
@@ -5,3 +5,4 @@ from torchsupport.modules.structured.pooling import *
 from torchsupport.modules.structured.spectral import *
 from torchsupport.modules.structured.entitynn import *
 from torchsupport.modules.structured.entitystructure import *
+from torchsupport.modules.structured.packedtensor import PackedTensor
