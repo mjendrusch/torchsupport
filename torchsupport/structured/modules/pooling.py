4eb8e65d67f3bf31dee7ff73bdeb2296004e0ba0
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2019-07-03 12:47:06 +0200

    multiple instance and transformer.

--- torchsupport/structured/modules/pooling.py
+++ torchsupport/structured/modules/pooling.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as func
 
 from torchsupport.structured import structures as cs
+from torchsupport.structured import scatter
 
 class DeletionPool(nn.Module):
   def __init__(self, size):
@@ -33,3 +34,22 @@ class GraphPool(nn.Module):
 
   def forward(self, nodes, indices):
     return self.combine(nodes, indices)
+
+class MILAttention(GraphPool):
+  def __init__(self, in_size, out_size, attention_size, heads):
+    super(MILAttention, self).__init__()
+    self.heads = heads
+    self.query = nn.Linear(attention_size, heads)
+    self.gate = nn.Linear(attention_size, heads)
+    self.key = nn.Linear(in_size, attention_size)
+    self.value = nn.Linear(in_size, heads * attention_size)
+    self.out = nn.Linear(heads * attention_size, out_size)
+
+  def combine(self, nodes, indices):
+    logits = self.query(torch.tanh(self.key(nodes)) * torch.sigmoid(self.gate(nodes)))
+    weight = scatter.softmax(logits, indices)
+    value = self.value(nodes)
+    weight = weight.view(*weight.shape[:-1], 1, self.heads)
+    value = value.view(*value.shape[:-1], -1, self.heads)
+    result = scatter.sum(weight * value)
+    return self.out(result.view(*result.shape[:-2], -1))
---
+++ torchsupport/structured/modules/transformer.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+
+from torchsupport.modules.basic import MLP
+from torchsupport.structured.modules.basic import NeighbourDotMultiHeadAttention
+
+class Transformer(nn.Module):
+  def __init__(self, in_size, out_size, hidden_size,
+               query_size=None, attention_size=64,
+               heads=8, mlp_depth=3, activation=func.elu_):
+    super(Transformer, self).__init__()
+    self.local = MLP(
+      in_size, hidden_size,
+      depth=mlp_depth,
+      batch_norm=False,
+      activation=activation
+    )
+    self.project_in = nn.Linear(in_size, out_size)
+    self.interact = NeighbourDotMultiHeadAttention(
+      hidden_size, out_size, attention_size,
+      query_size=query_size, heads=heads
+    )
+    self.local_bn = nn.LayerNorm(hidden_size)
+    self.residual_bn = nn.LayerNorm(hidden_size)
+    self.activation = activation
+
+  def forward(self, data, structure):
+    local = self.activation(self.local_bn(self.local(data)))
+    interaction = self.interact(local, structure)
+    return self.residual_bn(interaction + self.project_in(data))
