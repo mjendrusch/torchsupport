73fe0e459b5e28b86497bb913f07c43846857cdc
sholderbach <ho.steve@web.de>
2019-09-01 18:34:30 +0200

    [WIP] Implement RAdam
    * Simple adaptation of the pytorch Adam
    * Removed amsgrad functionality for now as the resulting code would be
    quite messy
    * Still open: true weight decay

---
+++ torchsupport/training/optim.py
@@ -0,0 +1,107 @@
+import torch
+from torch.optim import Optimizer
+
+class RAdam(Optimizer):
+    r"""Implements RAdam algorithm.
+
+    Uses modifications proposed in `On the Variance of the Adaptive Learning Rate and Beyond`_.
+    Drops the AMSgrad functionality for now, TODO: Reimplement nicely
+
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-3)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-8)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+            TODO: Make it actual weight decay
+
+    .. _Adam\: A Method for Stochastic Optimization:
+        https://arxiv.org/abs/1412.6980
+    .. _On the Convergence of Adam and Beyond:
+        https://openreview.net/forum?id=ryQu7f-RZ
+    """
+
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
+                 weight_decay=0):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 3/5 < betas[1] < 1.0: # Adapted to ensure RAdam stability
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        rho_inf = 2 / (1-betas[1]) - 1
+        assert rho_inf > 4, 'RAdam expects that the rho_inf value is greater 4'
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay, rho_inf=rho_inf)
+        super(RAdam, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+                step = state['step']
+                rho_inf = state['rho_inf']
+
+                step += 1
+
+                bias_correction1 = 1 - beta1 ** step
+                bias_correction2 = 1 - beta2 ** step
+                rho_step = rho_inf - 2 * step * beta2**step / (1 - beta2**step)
+
+                if group['weight_decay'] != 0:
+                    grad.add_(group['weight_decay'], p.data)
+
+                # Decay the first and second moment running average coefficient
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)               # m_t
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)  # v_t
+
+                if rho_step <= 4:
+                    step_size = group['lr'] / bias_correction1
+                    p.data.add_(-step_size, exp_avg)
+                else:
+                    r_factor = math.sqrt(
+                        ((rho_step - 4) * (rho_step - 2) * rho_inf)
+                        /
+                        ((rho_inf - 4) * (rho_inf - 2) * rho_step)
+                    )
+
+                    denom = exp_avg_sq.sqrt().add_(group['eps'])
+
+                    step_size = group['lr'] * r_factor * math.sqrt(bias_correction2) / bias_correction1
+
+                    p.data.addcdiv_(-step_size, exp_avg, denom)
+
+        return loss
+
