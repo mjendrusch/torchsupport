2daec662d8e38f6e574ea6d8dcb9e90ac4f1075b
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2019-09-02 13:14:33 +0200

    gradient estimator improvements.

---
+++ torchsupport/distributions/__init__.py
@@ -0,0 +1,45 @@
+import torch.distributions as dist
+
+def _harden_one_hot(self, inputs):
+  hard = torch.zeros_like(inputs)
+  hard_index = inputs.argmax(dim=-1)
+  hard[torch.arange(0, hard.size(0)), hard_index] = 1.0
+  return hard.detach()
+
+def _harden_bernoulli(self, inputs):
+  logits = torch.log(inputs / (1 - inputs + 1e-16) + 1e-16)
+  return (logits > 0).float().detach()
+
+def _hard_categorical(self, dist):
+  return dist.OneHotCategorical(logits=dist.logits)
+
+def _hard_bernoulli(self, dist):
+  return dist.Bernoulli(logits=dist.logits)
+
+def _condtitional_categorical(self, hard):
+  noise = -torch.log(torch.rand_like(self.logits) + 1e-16)
+  on_condition = noise * hard
+  off_condition = noise * (1 - hard)
+  offset = on_condition.view(-1, hard.size(-1)).sum(dim=-1).view(*hard.shape[:-1], 1)
+  off_condition = off_condition / (self.probs + 1e-16) - offset
+  soft_conditional = -torch.log(on_condition + off_condition + 1e-16)
+  return soft_conditional
+
+def _conditional_bernoulli(self, hard):
+  noise = torch.rand_like(hard)
+  on_condition = noise * hard
+  off_condition = noise * (1 - hard)
+  on_condition = on_condition * self.probs + (1 - self.probs)
+  off_condition = off_condition * (1 - self.probs)
+  total = on_condition + off_condition
+  soft_conditional = torch.log(self.probs / (1 - self.probs + 1e-16) + 1e-16)
+  soft_conditional += torch.log(total / (1 - total + 1e-16) + 1e-16)
+  return soft_conditional
+
+setattr(dist.RelaxedOneHotCategorical, "harden", _harden_one_hot)
+setattr(dist.RelaxedOneHotCategorical, "hard_distribution", _hard_categorical)
+setattr(dist.RelaxedOneHotCategorical, "conditional_rsample", _condtitional_categorical)
+
+setattr(dist.RelaxedBernoulli, "harden", _harden_bernoulli)
+setattr(dist.RelaxedBernoulli, "hard_distribution", _hard_bernoulli)
+setattr(dist.RelaxedBernoulli, "conditional_rsample", _conditional_bernoulli)
--- torchsupport/modules/gradient.py
+++ torchsupport/modules/gradient.py
@@ -1,18 +1,30 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as func
+import torchsupport.distributions as dist
 
 def sample(dist):
   value = dist.sample()
   logprob = dist.log_prob(value)
   return value, logprob
 
+def rsample(dist):
+  value = dist.rsample()
+  hard_value = dist.harden(value)
+  logprob = dist.log_prob(value)
+  hard_logprob = dist.hard_distribution().log_prob(hard_value)
+  return value, hard_value, logprob, hard_logprob
+
+def replace_gradient(value, gradient_provider):
+  result = value.detach()
+  return result + gradient_provider - gradient_provider.detach()
+
 def straight_through(op):
   def _func(*inputs, **kwargs):
     *result, = op(*inputs, **kwargs)
     surrogate = inputs
     result = [
-      r.detach() + s - s.detach()
+      replace_gradient(r, s)
       for (r, s) in zip(result, surrogate)
     ]
     if len(result) > 1:
@@ -23,11 +35,8 @@ def straight_through(op):
 def hard_distribution(distribution, logits, **kwargs):
   dist = distribution(logits=logits, **kwargs)
   surrogate = dist.rsample()
-  result_index = surrogate.argmax(dim=1)
-  result = torch.zeros_like(logits)
-  result[torch.arange(0, result.size(0)), result_index] = 1.0
-  result = result.detach()
-  return result + surrogate - surrogate.detach()
+  result = dist.harden(surrogate)
+  return replace_gradient(result, surrogate)
 
 def hard_one_hot(logits, temperature=0.1):
   return hard_distribution(
@@ -43,6 +52,40 @@ def hard_bernouilli(logits, temperature=0.1):
     temperature=temperature
   )
 
+def _k_hot_sample_batch(weights):
+    uniform = torch.rand_like(weights)
+    gumbel = -torch.log(-torch.log(uniform + 1e-20) + 1e-20) + torch.log(weights + 1e-20)
+    return gumbel
+
+def _k_hot_compute_probability(alpha, temperature=0.1):
+  alpha_linear = alpha.view(alpha.size(0), alpha.size(1), -1)
+  result = torch.softmax(alpha_linear / temperature, dim=-1)
+  return result.view(*alpha.size())
+
+def _k_hot_relaxed_top_k(batch, k=1, temperature=0.1):
+  alpha_idx = batch
+  probability = _k_hot_compute_probability(alpha_idx, temperature=temperature)
+  result = probability
+  for _ in range(1, k):
+    alpha_idx = alpha_idx + torch.log(1 - probability + 1e-20)
+    probability = _k_hot_compute_probability(alpha_idx, temperature=temperature)
+    result = result + probability
+  return result
+
+def soft_k_hot(logits, k, temperature=0.1):
+  weights = torch.sigmoid(logits)
+  batch = _k_hot_sample_batch(weights)
+  top_k = _k_hot_relaxed_top_k(batch, k=k, temperature=temperature)
+  return top_k
+
+def hard_k_hot(logits, k, temperature=0.1):
+  soft = soft_k_hot(logits, k, temperature=temperature)
+  hard = torch.zeros_like(soft)
+  _, top_k = torch.topk(logits, k)
+  index = torch.repeat_interleave(torch.arange(0, hard.size(0)), k)
+  hard[index, top_k.view(-1)] = 1.0
+  return replace_gradient(hard, soft)
+
 def reinforce(op, sample=sample):
   def _func(*inputs, **kwargs):
     samples = []
@@ -54,7 +97,7 @@ def reinforce(op, sample=sample):
     logprob = sum(logprobs)
     result = op(*samples, **kwargs).detach()
     surrogate = result * logprob
-    return result + surrogate - surrogate.detach()
+    return replace_gradient(result, surrogate)
   return _func
 
 class Reinforce(nn.Module):
@@ -66,30 +109,53 @@ class Reinforce(nn.Module):
     return self.target(inputs)
 
 ## TODO: implement Rebar, Relax, DiCE etc.
-# class Lax(nn.Module):
-#   def __init__(self, target, sample=sample, joint=False):
-#     super(Lax, self).__init__()
-#     self.target = target
-#     self.sample = sample
-#     self.joint = joint
-#     self.value = ...
+class Lax(nn.Module):
+  def __init__(self, target, value, sample=rsample, joint=False):
+    super(Lax, self).__init__()
+    self.target = target
+    self.sample = sample
+    self.joint = joint
+    self.value = value
 
-#   def value_loss(self, inputs=None, forward=None):
-#     if inputs is not None:
-#       result = self.forward(inputs)
-#       loss = torch.autograd.grad(result, self.target.parameters(),
-#                                  retain_graph=True, create_graph=True) ** 2
-#       return loss
-#     elif forward is not None:
-#       return torch.autograd.grad(result, self.target.parameters(),
-#                                  retain_graph=True, create_graph=True) ** 2
-#     else:
-#       raise ValueError("Either inputs or forward must be not None.")
+  def value_loss(self, inputs=None, forward=None):
+    if inputs is not None:
+      result = self.forward(inputs)
+      loss = torch.autograd.grad(result, self.target.parameters(),
+                                 retain_graph=True, create_graph=True) ** 2
+      return loss
+    elif forward is not None:
+      return torch.autograd.grad(result, self.target.parameters(),
+                                 retain_graph=True, create_graph=True) ** 2
+    else:
+      raise ValueError("Either inputs or forward must be not None.")
 
-#   def forward(self, inputs):
-#     sample = self.sample(inputs)
-#     result = (self.target(sample) - self.value(inputs)).detach() * torch.log(inputs)
-#     result = result + self.value(sample)
-#     if self.joint:
-#       return result, self.value_loss(forward=result)
-#     return result
+  def forward(self, inputs):
+    sample, hard, logprob, hard_logprob = self.sample(inputs)
+    result = self.target(hard)
+    control = self.value(sample)
+    surrogate = result.detach() * hard_logprob - control.detach() * logprob + control
+    result = replace_gradient(result, surrogate)
+    if self.joint:
+      return result, self.value_loss(forward=result)
+    return result
+
+class Relax(Lax):
+  def __init__(self, target, value, sample=rsample, joint=False):
+    super(Relax, self).__init__(
+      target, value,
+      sample=sample,
+      joint=joint
+    )
+
+  def forward(self, inputs):
+    sample, hard_sample, logprob, hard_logprob = self.sample(inputs)
+    conditional_sample = inputs.conditional_sample(hard_sample)
+    result = self.target(hard_sample)
+    control = self.value(sample)
+    conditional_control = self.value(conditional_sample)
+    surrogate = (result.detach() - conditional_control.detach()) * hard_logprob
+    surrogate += control - conditional_control
+    result = replace_gradient(result, surrogate)
+    if self.joint:
+      return result, self.value_loss(forward=result)
+    return result
