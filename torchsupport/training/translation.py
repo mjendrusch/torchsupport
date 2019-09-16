b08e6926f4d876e4b92540a083afe728996be36e
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2019-09-16 21:54:02 +0200

    [WIP] better CycleGAN.

--- torchsupport/training/translation.py
+++ torchsupport/training/translation.py
@@ -97,3 +97,129 @@ class CycleGANTraining(RothGANTraining):
     fake_batch = fake_fw, fake_rv
 
     return fake_batch, data, fake_result, real_result
+
+class AugmentedCycleGANTraining(CycleGANTraining):
+  def __init__(self, generators, discriminators, encoders, data, gamma=10, **kwargs):
+    self.fw = ...
+    self.rv = ...
+    self.fw_encoder = ...
+    self.rv_encoder = ...
+    self.fw_discriminator = ...
+    self.rv_discriminator = ...
+    self.z_fw_discriminator = ...
+    self.z_rv_discriminator = ...
+    self.discriminator = ...
+    AbstractGANTraining.__init__(
+      {
+        "fw": generators[0],
+        "rv": generators[1],
+        "fw_encoder": encoders[0],
+        "rv_encoder": encoders[1]
+      },
+      {
+        "fw_discriminator": discriminators[0],
+        "rv_discriminator": discriminators[1],
+        "z_fw_discriminator": discriminators[2],
+        "z_rv_discriminator": discriminators[3]
+      },
+      data, **kwargs
+    )
+    self.generator = self.fw
+
+    self.gamma = gamma
+
+  def run_cycle(self, data, fw, rv, fw_enc, rv_enc):
+    z_fw = fw_enc.sample(self.batch_size)
+    translated_fw = fw(z_fw, data)
+    z_rv = rv_enc(data, translated_fw)
+    cycled_fw = rv(z_rv, translated_fw)
+    z_cycled_fw = fw_enc(data, translated_fw)
+
+    return data, translated_fw, cycled_fw, z_fw, z_rv, z_cycled_fw
+
+  def generator_step_loss(self, data, translated, cycled, z_fw, z_rv, z_cycled):
+    self.set_discriminator(self.fw_discriminator)
+    loss_fw = self.generator_loss(data[0], translated[0])
+    loss_cycle_fw = self.cycle_loss(data[0], cycled[0])
+
+    self.set_discriminator(self.z_fw_discriminator)
+    loss_z_fw = self.generator_loss(z_fw[0], z_rv[0])
+    loss_z_cycle_fw = self.cycle_loss(z_fw[0], z_cycled[0])
+
+    self.set_discriminator(self.rv_discriminator)
+    loss_rv = self.generator_loss(data[1], translated[1])
+    loss_cycle_rv = self.cycle_loss(data[1], cycled[1])
+
+    self.set_discriminator(self.z_rv_discriminator)
+    loss_z_rv = self.generator_loss(z_fw[1], z_rv[1])
+    loss_z_cycle_rv = self.cycle_loss(z_fw[1], z_cycled[1])
+
+    loss_gan = loss_fw + loss_rv + loss_z_fw + loss_z_rv
+    loss_cycle = loss_cycle_fw + loss_cycle_rv + loss_z_cycle_fw + loss_z_cycle_rv
+
+    self.current_losses["cycle"] = float(loss_cycle)
+    self.current_losses["gan"] = float(loss_gan)
+
+    return loss_gan + self.gamma * loss_cycle
+
+  def discriminator_step_loss(self, translated, data, translated_result, real_result):
+    self.set_discriminator(self.fw_discriminator)
+    loss_fw, out_fw = self.discriminator_loss(
+      translated[0], data[0], translated_result[0], real_result[0]
+    )
+    self.current_losses["fw_discriminator"] = float(loss_fw)
+
+    self.set_discriminator(self.rv_discriminator)
+    loss_rv, out_rv = self.discriminator_loss(
+      translated[1], data[1], translated_result[1], real_result[1]
+    )
+    self.current_losses["rv_discriminator"] = float(loss_rv)
+
+    self.set_discriminator(self.z_fw_discriminator)
+    loss_z_fw, out_z_fw = self.discriminator_loss(
+      translated[2], data[2], translated_result[2], real_result[2]
+    )
+    self.current_losses["z_fw_discriminator"] = float(loss_fw)
+
+    self.set_discriminator(self.z_rv_discriminator)
+    loss_z_rv, out_z_rv = self.discriminator_loss(
+      translated[3], data[3], translated_result[3], real_result[3]
+    )
+    self.current_losses["z_rv_discriminator"] = float(loss_rv)
+
+    loss = loss_fw + loss_rv + loss_z_fw + loss_z_rv
+    out = (out_fw, out_rv, out_z_fw, out_z_rv)
+
+    self.current_losses["discriminator"] = float(loss)
+
+    return loss, out
+
+  def run_generator(self, data):
+    args_fw = self.run_cycle(
+      data[0], self.fw, self.rv, self.fw_encoder, self.rv_encoder
+    )
+    args_rv = self.run_cycle(
+      data[1], self.rv, self.fw, self.rv_encoder, self.fw_encoder
+    )
+    return tuple(zip(args_fw, args_rv))    
+
+  def run_discriminator(self, data):
+    with torch.no_grad():
+      _, (fake_fw, fake_rv), _, (real_z_fw, real_z_rv), (fake_z_fw, fake_z_rv)  = \
+        self.run_generator(data)
+    make_differentiable((fake_fw, fake_rv))
+    make_differentiable(data)
+    real_result_fw = self.fw_discriminator(data[1])
+    fake_result_fw = self.fw_discriminator(fake_fw)
+    real_result_rv = self.rv_discriminator(data[0])
+    fake_result_rv = self.rv_discriminator(fake_rv)
+    real_result_z_fw = self.z_fw_discriminator(real_z_fw)
+    fake_result_z_fw = self.z_fw_discriminator(fake_z_fw)
+    real_result_z_rv = self.z_rv_discriminator(real_z_rv)
+    fake_result_z_rv = self.z_rv_discriminator(fake_z_rv)
+
+    real_result = (real_result_fw, real_result_rv, real_result_z_fw, real_result_z_rv)
+    fake_result = (fake_result_fw, fake_result_rv, fake_result_z_fw, fake_result_z_rv)
+    fake_batch = fake_fw, fake_rv
+
+    return fake_batch, data, fake_result, real_result
