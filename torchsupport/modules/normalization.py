f010801209aa27f6f4fa4c1b4e430acd46d9a090
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2019-12-02 16:19:01 +0100

    add filter response norm.

--- torchsupport/modules/normalization.py
+++ torchsupport/modules/normalization.py
@@ -73,3 +73,53 @@ class AdaptiveLayerNorm(nn.Module):
     bias = self.bias(style).view(style.size(0), -1, 1, 1)
     bias = bias - bias.mean(dim=1, keepdim=True)
     return scale * (inputs - mean) / (std + 1e-6) + bias
+
+class FilterResponseNorm(nn.Module):
+  def __init__(self, in_size, eps=1e-16):
+    super().__init__()
+    self.eps = eps
+    self.in_size = in_size
+    self.register_parameter(
+      "scale",
+      nn.Parameter(torch.tensor(1.0, dtype=torch.float))
+    )
+    self.register_parameter(
+      "bias",
+      nn.Parameter(torch.tensor(0.0, dtype=torch.float))
+    )
+    self.register_parameter(
+      "threshold",
+      nn.Parameter(torch.tensor(0.0, dtype=torch.float))
+    )
+
+  def forward(self, inputs):
+    out = inputs.view(inputs.size(0), inputs.size(1), -1)
+    nu2 = out.mean(dim=-1)
+    extension = [1] * (inputs.dim() - 2)
+    denominator = torch.sqrt(nu2 + self.eps)
+    denominator = denominator.view(inputs.size(0), inputs.size(1), *extension)
+    out = inputs / denominator
+    out = func.relu(self.scale * out + self.bias - self.threshold) + self.threshold
+    return out
+
+class AdaptiveFilterResponseNorm(nn.Module):
+  def __init__(self, in_size, ada_size, eps=1e-16):
+    super().__init__()
+    self.eps = eps
+    self.in_size = in_size
+    self.scale = nn.Linear(ada_size, in_size)
+    self.bias = nn.Linear(ada_size, in_size)
+    self.threshold = nn.Linear(ada_size, in_size)
+
+  def forward(self, inputs, condition):
+    out = inputs.view(inputs.size(0), inputs.size(1), -1)
+    nu2 = out.mean(dim=-1)
+    extension = [1] * (inputs.dim() - 2)
+    denominator = torch.sqrt(nu2 + self.eps)
+    denominator = denominator.view(inputs.size(0), inputs.size(1), *extension)
+    out = inputs / denominator
+    scale = self.scale(condition)
+    bias = self.bias(condition)
+    threshold = self.threshold(condition)
+    out = func.relu(scale * out + bias - threshold) + threshold
+    return out
