5707ae6557b2f75ac42cf35b4fa78e3965173a1c
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2020-01-03 13:47:09 +0100

    fix sampling on regularized EBMs.

--- torchsupport/training/energy.py
+++ torchsupport/training/energy.py
@@ -235,7 +235,9 @@ class EnergyTraining(AbstractEnergyTraining):
   def sample(self):
     buffer_iter = iter(self.buffer_loader(self.buffer))
     data, *args = to_device(self.data_key(next(buffer_iter)), self.device)
+    self.score.eval()
     data = self.integrator.integrate(self.score, data, *args).detach()
+    self.score.train()
     detached = data.detach().cpu()
     update = (to_device((detached[idx], *[arg[idx] for arg in args]), "cpu") for idx in range(data.size(0)))
     make_differentiable(update, toggle=False)
