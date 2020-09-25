67e8c9914def68e9590a9c185732b7067d30511c
bobelly <bobkova@stud.uni-heidelberg.de>
2020-09-25 18:16:50 +0200

    Better Bobs_stuff

--- torchsupport/modules/rezero.py
+++ torchsupport/modules/rezero.py
@@ -4,15 +4,20 @@ import torch.nn.functional as func
 
 class ReZero(nn.Module):
   r'''Implemets ReZero normalization proposed by Bachlechner et al. 
-    (https://arxiv.org/pdf/2003.04887.pdf) 
+  (https://arxiv.org/pdf/2003.04887.pdf) 
   Args: 
-    function (callable): function you want to use (for example conv2D)'''
-  def __init__(self, function):
+    function (callable): function you want to use (for example conv2D)
+    out_size (int): dimension of the channel output'''
+  def __init__(self, function, out_size=1):
     super().__init__()
+    self.out_size = out_size
     self.function = function
-    self.alpha = nn.Parameter(torch.tensor(
-      0, dtype=torch.float, requires_grad=True
+    self.alpha = nn.Parameter(torch.zeros(
+      self.out_size, dtype=torch.float, requires_grad=True
     ))
 
   def forward(self, inputs):
-    return inputs + self.alpha * self.function(inputs)
+    dimension = inputs.dim() - 2
+    alpha = self.alpha[[None, slice(None)] + dimension * [None]]
+    return inputs + alpha * self.function(inputs)
+
