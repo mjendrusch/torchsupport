1f39fde83e0303044c4d704096563526c4612c84
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2020-09-25 14:44:59 +0200

    fixup DCGAN backbone.

--- torchsupport/modules/backbones/gan/dcgan.py
+++ torchsupport/modules/backbones/gan/dcgan.py
@@ -4,15 +4,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as func
 
-def _generator_layer(position, in_channels, out_channels, kernel_size, activation):
+def _generator_layer(position, in_channels, out_channels, kernel_size, activation,
+                     expand_first=False):
+  expand_first = expand_first and position
   return nn.Sequential(
     nn.ConvTranspose2d(
       in_channels,
       out_channels,
       kernel_size,
-      padding=kernel_size // 2 if position else 0,
-      output_padding=1 if position else 0,
-      stride=2
+      padding=kernel_size // 2 if expand_first else 0,
+      output_padding=1 if expand_first else 0,
+      stride=2 if expand_first else 1
     ),
     nn.BatchNorm2d(out_channels),
     activation
@@ -30,17 +32,23 @@ def _discriminator_layer(in_channels, out_channels, kernel_size, activation):
 
 class DCGANGenerator(nn.Module):
   def __init__(self, in_size=100, base_channels=64, channel_factors=None,
-               kernel_size=5, activation=None):
+               kernel_size=5, initial_size=4, activation=None):
     super().__init__()
+    expand_first = (kernel_size - 1 + initial_size) == 2 * initial_size
+    self.initial_size = initial_size
     self.activation = activation or nn.ReLU()
-    self.initial = nn.Linear(in_size, 4 * 4 * base_channels * channel_factors[0])
+    self.initial = nn.Linear(
+      in_size,
+      self.initial_size ** 2 * base_channels * channel_factors[0]
+    )
     self.blocks = nn.ModuleList([
       _generator_layer(
         position=idx,
         in_channels=in_factor * base_channels,
         out_channels=out_factor * base_channels,
         kernel_size=kernel_size,
-        activation=self.activation
+        activation=self.activation,
+        expand_first=expand_first
       )
       for idx, (in_factor, out_factor) in enumerate(zip(
         channel_factors[:-1], channel_factors[1:]
@@ -48,7 +56,11 @@ class DCGANGenerator(nn.Module):
     ])
 
   def forward(self, inputs):
-    out = self.initial(inputs).view(inputs.size(0), -1, 4, 4)
+    out = self.initial(inputs).view(
+      inputs.size(0), -1,
+      self.initial_size,
+      self.initial_size
+    )
     for block in self.blocks:
       out = block(out)
     return out
