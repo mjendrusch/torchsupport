ea9a69ce1944c0c707c039ded372f2a8bd36013f
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2020-10-12 12:45:06 +0200

    add missing imports.

--- torchsupport/structured/modules/__init__.py
+++ torchsupport/structured/modules/__init__.py
@@ -3,3 +3,5 @@ from .parallel import *
 from .pooling import *
 from .spectral import *
 from .transformer import *
+from .sequence_transformer import *
+from .rezero_transformer import RezeroTransformerBlock
