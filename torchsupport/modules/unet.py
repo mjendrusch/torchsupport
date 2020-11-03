f74a9ab81ee2b60f552b47ecb9f768a66b5f1dc4
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2020-11-03 19:32:57 +0100

    update UNet backbones.

--- torchsupport/modules/unet.py
+++ torchsupport/modules/unet.py
@@ -36,7 +36,7 @@ class NestedWrapper(NestedModule):
     inputs = torch.cat((inputs, skip), dim=1)
     return self.right(inputs, *args, **kwargs)
 
-class ResUNetBlock(NestedModule):
+class LightResUNetBlock(NestedModule):
   def __init__(self, hole, in_size=64, out_size=64,
                hidden_size=64, kernel_size=3, dilation=1,
                padding=None, depth=3, downscale=2,
@@ -50,6 +50,7 @@ class ResUNetBlock(NestedModule):
     self.downscale = downscale
     self.into_preprocess = nn.Conv2d(in_size, hidden_size, 1)
     self.into_postprocess = nn.Conv2d(hidden_size, out_size, 1)
+    self.into_bn = nn.InstanceNorm2d(in_size)
     self.into_blocks = nn.ModuleList([
       nn.Conv2d(
         hidden_size,
@@ -66,6 +67,7 @@ class ResUNetBlock(NestedModule):
     self.outof_preprocess = nn.Conv2d(
       out_size + hidden_size, hidden_size, 1
     )
+    self.outof_bn = nn.InstanceNorm2d(out_size + hidden_size)
     self.outof_postprocess = nn.Conv2d(hidden_size, in_size, 1)
     self.outof_blocks = nn.ModuleList([
       nn.Conv2d(
@@ -82,14 +84,15 @@ class ResUNetBlock(NestedModule):
     ])
 
   def enter(self, inputs, *args, **kwargs):
-    out = self.activation(self.into_preprocess(inputs))
-    if self.cond:
-      cond = torch.cat(args, dim=1)
-      cond = self.cond(cond)[:, :, None, None]
-      out = out + cond
+    out = self.activation(self.into_preprocess(self.into_bn(inputs)))
     for zero, block in zip(self.into_zeros, self.into_blocks):
+      this_out = out
+      if self.cond:
+        cond = torch.cat(args, dim=1)
+        cond = self.cond(cond)[:, :, None, None]
+        this_out = this_out + cond
       res = func.dropout(
-        self.activation(block(out)),
+        self.activation(block(this_out)),
         0.1, training=self.training
       )
       out = zero(out, res)
@@ -102,14 +105,15 @@ class ResUNetBlock(NestedModule):
   def exit(self, inputs, skip, *args, **kwargs):
     if self.downscale != 1:
       inputs = func.interpolate(inputs, scale_factor=self.downscale, mode="bilinear")
-    out = self.activation(self.outof_preprocess(torch.cat((inputs, skip), dim=1)))
-    if self.cond:
-      cond = torch.cat(args, dim=1)
-      cond = self.cond(cond)[:, :, None, None]
-      out = out + cond
+    out = self.activation(self.outof_preprocess(self.outof_bn(torch.cat((inputs, skip), dim=1))))
     for zero, block in zip(self.outof_zeros, self.outof_blocks):
+      this_out = out
+      if self.cond:
+        cond = torch.cat(args, dim=1)
+        cond = self.cond(cond)[:, :, None, None]
+        this_out = this_out + cond
       res = func.dropout(
-        self.activation(block(out)),
+        self.activation(block(this_out)),
         0.1, training=self.training
       )
       out = zero(out, res)
@@ -123,8 +127,136 @@ def nest_sequential(*args):
     block = item
   return block
 
+class Ignore(nn.Module):
+  def forward(self, x, *args, **kwargs):
+    return x
+
+class IgnoreArgs(nn.Module):
+  def __init__(self, net):
+    super().__init__()
+    self.net = net
+
+  def forward(self, x, *args, **kwargs):
+    return self.net(x)
+
+class LightUNetBackbone(nn.Module):
+  def __init__(self, hole=None, size_factors=None,
+               kernel_size=None, base_size=64, **kwargs):
+    super().__init__()
+    kernel_size = kernel_size or 3
+    self.blocks = nest_sequential(*([
+      LightResUNetBlock(
+        None, in_size=base_size * in_factor,
+        kernel_size=kernel_size,
+        out_size=base_size * out_factor,
+        **kwargs
+      )
+      for in_factor, out_factor in zip(
+        size_factors[:-1], size_factors[1:]
+      )
+    ] + [
+      hole or Ignore()
+    ]))
+
+  def forward(self, inputs, *args, **kwargs):
+    return self.blocks(inputs, *args, **kwargs)
+
+
+class ResSubblock(nn.Module):
+  def __init__(self, in_size=64, out_size=64, cond_size=None,
+               kernel_size=3, dilation=1, padding=None,
+               activation=None, norm=None):
+    super().__init__()
+    padding = padding or (kernel_size // 2 * dilation)
+    self.cond = None
+    self.norm = norm or (lambda x: nn.Identity())
+    self.activation = activation or func.relu
+    if cond_size:
+      self.cond = nn.Linear(cond_size, out_size)
+    self.blocks = nn.ModuleList([
+      nn.Conv2d(in_size, out_size, kernel_size, dilation, padding),
+      nn.Conv2d(out_size, out_size, kernel_size, dilation, padding)
+    ])
+    self.project = nn.Conv2d(in_size, out_size, 1, bias=False)
+    self.zero = ReZero(out_size)
+    self.norms = nn.ModuleList([
+      self.norm(in_size),
+      self.norm(out_size)
+    ])
+
+  def forward(self, inputs, cond=None):
+    out = inputs
+    out = self.blocks[0](self.activation(self.norms[0](out)))  
+    if cond is not None:
+      out = out + self.cond(cond)[:, :, None, None]
+    out = func.dropout(
+      self.activation(self.norms[1](out)),
+      0.1, training=self.training
+    )
+    out = self.blocks[1](out)
+    out = self.zero(self.project(inputs), out)
+    return out
+
+class ResUNetBlock(NestedModule):
+  def __init__(self, hole, in_size=64, out_size=64,
+               hidden_size=64, kernel_size=3, dilation=1,
+               padding=None, depth=2, downscale=2,
+               cond_size=None, activation=None, norm=None):
+    super().__init__(hole)
+    self.activation = activation or func.relu
+    self.downscale = downscale
+    self.into_blocks = nn.ModuleList([
+      ResSubblock(
+        in_size, in_size, cond_size=cond_size,
+        kernel_size=kernel_size, dilation=dilation,
+        padding=padding, activation=activation,
+        norm=norm
+      )
+      for idx in range(depth - 1)
+    ] + [
+      ResSubblock(
+        in_size, out_size, cond_size=cond_size,
+        kernel_size=kernel_size, dilation=dilation,
+        padding=padding, activation=activation,
+        norm=norm
+      )
+    ])
+    self.outof_blocks = nn.ModuleList([
+      ResSubblock(
+        2 * out_size, in_size, cond_size=cond_size,
+        kernel_size=kernel_size, dilation=dilation,
+        padding=padding, activation=activation,
+        norm=norm
+      )
+    ] + [
+      ResSubblock(
+        in_size, in_size, cond_size=cond_size,
+        kernel_size=kernel_size, dilation=dilation,
+        padding=padding, activation=activation,
+        norm=norm
+      )
+      for idx in range(depth - 1)
+    ])
+
+  def enter(self, inputs, cond=None):
+    out = inputs
+    for block in self.into_blocks:
+      out = block(out, cond=cond)
+    skip = out
+    if self.downscale != 1:
+      out = func.avg_pool2d(out, self.downscale)
+    return out, skip
+
+  def exit(self, inputs, skip, cond=None):
+    if self.downscale != 1:
+      inputs = func.interpolate(inputs, scale_factor=self.downscale, mode="nearest")
+    out = torch.cat((inputs, skip), dim=1)
+    for block in self.outof_blocks:
+      out = block(out, cond=cond)
+    return out
+
 class UNetBackbone(nn.Module):
-  def __init__(self, size_factors=None, kernel_size=None, base_size=64, **kwargs):
+  def __init__(self, hole=None, size_factors=None, kernel_size=None, base_size=64, **kwargs):
     super().__init__()
     kernel_size = kernel_size or 3
     self.blocks = nest_sequential(*([
@@ -138,7 +270,7 @@ class UNetBackbone(nn.Module):
         size_factors[:-1], size_factors[1:]
       )
     ] + [
-      nn.Identity()
+      hole or Ignore()
     ]))
 
   def forward(self, inputs, *args, **kwargs):
