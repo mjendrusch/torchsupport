4e9e6c0977deb7266c4b66e4e8812be1a2c930c5
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2020-12-08 14:26:21 +0100

    fixup weight demodulation.

--- torchsupport/modules/weights.py
+++ torchsupport/modules/weights.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as func
 
+from torchsupport.modules.dynamic import (
+  dynamic_linear, dynamic_conv1d, dynamic_conv2d, dynamic_conv3d
+)
+
 def initialize(module, **kwargs):
   """Performs per-parameter initialization on a given module.
   Module parameters can be addressed by their name in the keyword arguments,
@@ -72,32 +76,43 @@ def lr_equal(module, lr_scale=1.0):
   return module
 
 class WeightDemodulation(LREqualization):
-  """Equalizes the learning rate of a given module at run time
-    as applied in the ProGAN and StyleGAN architectures.
+  conv_names = [
+    "stride", "kernel_size", "dilation", "padding"
+  ]
 
-  Args:
-    modules (nn.Module): module to regularise by learning rate normalization.
-    gain (float): Gain as used in He normal initialization.
-    lr_scale (float): factor by which to multiply the module's learning rate.
-
-  Returns:
-    A wrapper module performing the forward pass of the input module with
-    learning rate equalization and scaling.
-  """
   def __init__(self, module, gain=2.0, lr_scale=1.0, epsilon=1e-6):
     super().__init__(module, gain=gain, lr_scale=lr_scale)
     self.epsilon = epsilon
+    self.func = ...
+    self.kwargs = {}
+    if isinstance(self.module, nn.Linear):
+      self.func = dynamic_linear
+    elif isinstance(self.module, nn.Conv1d):
+      self.func = dynamic_conv1d
+      for name in self.conv_names:
+        self.kwargs[name] = getattr(module, name)
+    elif isinstance(self.module, nn.Conv2d):
+      self.func = dynamic_conv2d
+      for name in self.conv_names:
+        self.kwargs[name] = getattr(module, name)
+    elif isinstance(self.module, nn.Conv3d):
+      self.func = dynamic_conv3d
+      for name in self.conv_names:
+        self.kwargs[name] = getattr(module, name)
+    else:
+      raise NotImplementedError(
+        "Weight demodulation is only implemented for Linear and Conv layers."
+      )
 
   def forward(self, inputs, condition):
     weight, bias = lr_equal_weight(
       self.weight, self.bias, gain=self.gain, lr_scale=self.lr_scale
     )
-    weight = demodulized_weight(weight, condition, epsilon=self.epsilon)
-    self.module.weight = weight
-    self.module.bias = bias
-    return self.module(inputs)
+    weight = demodulated_weight(weight, condition, epsilon=self.epsilon)
+    bias = bias[None].expand(weight.size(0), bias.size(0))
+    return self.func(inputs, weight, bias, **self.kwargs)
 
-def demodulized_weight(weight, condition, epsilon=1e-6):
+def demodulated_weight(weight, condition, epsilon=1e-6):
   weight = weight[None] * condition[:, None, :, None, None]
   sigma = (weight ** 2).view(*weight.shape[:2], -1).sum(dim=-1)
   sigma = (sigma + epsilon).sqrt()
