2cc7ea8901f861a29a3b81ea8eb3808cf1c7375b
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2021-03-16 11:16:28 +0100

    fixup distribution modifiers.

--- torchsupport/distributions/modifiers.py
+++ torchsupport/distributions/modifiers.py
@@ -1,16 +1,17 @@
+import types
 import torch
 from torchsupport.modules.gradient import replace_gradient
 
 def fixed(distribution, sample):
   def return_sample(dist, sample_shape=torch.Size()):
     return sample
-  distribution.rsample = return_sample
-  distribution.sample = return_sample
+  distribution.rsample = types.MethodType(return_sample, distribution)
+  distribution.sample = types.MethodType(return_sample, distribution)
   return distribution
 
 def hardened(distribution):
   def harden_sample(dist, sample_shape=torch.Size()):
     result = dist.rsample(sample_shape=sample_shape)
     return replace_gradient(dist.harden(result), result)
-  distribution.rsample = harden_sample
-  distribution.sample = harden_sample
+  distribution.rsample = types.MethodType(harden_sample, distribution)
+  distribution.sample = types.MethodType(harden_sample, distribution)
